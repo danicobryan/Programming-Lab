b3b5134e36dd6ebe5bce5a0e671316410f9bd517
danicobryan <danicobryan@gmail.com>
2016-05-23 18:35:13 -0700

    Create Shape.java

---
+++ Randomized-Estimation/Shape.java
@@ -0,0 +1,11 @@
+public abstract class Shape{
+	//public abstract double area();
+	//public abstract double getPerimeter();
+	public abstract double getXMax();
+	public abstract double getXMin();
+	public abstract double getYMax();
+	public abstract double getYMin();
+	//public abstract Rectangle boundingBox();
+	public abstract boolean pointInArea(Point p); 
+
+}
